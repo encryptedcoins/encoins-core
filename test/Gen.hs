4bd730a259beff969ac1df606cd4715c0c6fdf55
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-07-04 22:37:45 +0300

    #3

--- test/Gen.hs
+++ test/Gen.hs
@@ -2,12 +2,17 @@
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TupleSections              #-}
 
 module Gen where
 
-import           Control.Monad          (replicateM)
-import           Test.QuickCheck        (Arbitrary (..), choose)
+import           Control.Monad        (replicateM)
+import           ENCOINS.Core.OnChain (EncoinsProtocolParams)
+import           Plutus.V2.Ledger.Api (BuiltinByteString)
+import           Test.QuickCheck      (Arbitrary (..), choose, generate)
 
+genEncoinsParams :: BuiltinByteString -> IO EncoinsProtocolParams
+genEncoinsParams verifierPKH = generate $ (,,verifierPKH) <$> arbitrary <*> arbitrary
 class EncoinsRequest a where
     extractRequest :: a -> [Integer]
 
@@ -21,7 +26,7 @@ newtype MintRequest = MintRequest [Integer]
 instance Arbitrary MintRequest where
     arbitrary = do
         MixedRequest req <- arbitrary
-        pure $ MintRequest $ map abs req
+        pure $ MintRequest $ map abs $ filter (/= 0) req
 
 newtype BurnRequest = BurnRequest [Integer]
     deriving (Show, Eq)
@@ -30,7 +35,7 @@ newtype BurnRequest = BurnRequest [Integer]
 instance Arbitrary BurnRequest where
     arbitrary = do
         MintRequest req <- arbitrary
-        pure $ BurnRequest $ map negate req
+        pure $ BurnRequest $ map negate $ filter (/= 0) req
 
 newtype MixedRequest = MixedRequest [Integer]
     deriving (Show, Eq)
--- test/Script.hs
+++ test/Script.hs
@@ -9,34 +9,33 @@ module Script where
 
 import           Cardano.Api                (NetworkId (..), NetworkMagic (..))
 import           Cardano.Node.Emulator      (Params (..), pParamsFromProtocolParams)
+import           Control.Monad              (replicateM)
 import           Data.Aeson                 (decode, eitherDecodeFileStrict)
+import           Data.Bifunctor             (Bifunctor (..))
 import           Data.ByteString.Lazy       (readFile)
 import           Data.Default               (def)
-import           Data.Maybe                 (fromJust)
-import           ENCOINS.Core.OnChain
-import           Plutus.V2.Ledger.Api
-import           PlutusTx.AssocMap          (empty)
-import qualified PlutusTx.AssocMap          as PAM
-import           PlutusTx.Prelude           (emptyByteString, sha2_256, zero)
-import           Prelude                    hiding (readFile)
-
-import           PlutusAppsExtra.Test.Utils (testMintingPolicy, testValidator, genPubKeyAddress)
-
-import           Control.Monad              (replicateM, join)
-import           Data.Bifunctor             (Bifunctor (..))
 import           Data.Function              (on)
-import           Data.List                  (sortBy, partition)
+import           Data.Functor               ((<&>))
+import           Data.List                  (sortBy)
+import           Data.Maybe                 (fromJust)
 import           ENCOINS.BaseTypes          (MintingPolarity (..))
 import           ENCOINS.Bulletproofs       (Secret (Secret), bulletproof, fromSecret, parseBulletproofParams, polarityToInteger)
-import           ENCOINS.Core.OffChain      (EncoinsMode (..), mkEncoinsRedeemerOnChain, protocolFee, protocolFeeValue)
+import           ENCOINS.Core.OffChain      (EncoinsMode (..), mkEncoinsRedeemerOnChain, protocolFee)
+import           ENCOINS.Core.OnChain
 import           ENCOINS.Crypto.Field       (toFieldElement)
-import           Ledger.Ada                 (lovelaceValueOf, adaValueOf)
+import           Gen                        (BurnRequest, EncoinsRequest (..), MintRequest (..), MixedRequest, genEncoinsParams)
+import           Ledger.Ada                 (lovelaceValueOf)
+import           Ledger.Value               (adaOnlyValue, isZero, leq)
+import           Plutus.V2.Ledger.Api
+import           PlutusAppsExtra.Test.Utils (emptyInfo, genPubKeyAddress, testMintingPolicy, testValidator)
+import           PlutusTx.AssocMap          (empty)
+import qualified PlutusTx.AssocMap          as PAM
 import           PlutusTx.Extra.ByteString  (ToBuiltinByteString (..))
-import           System.Random              (randomIO, randomRIO)
-import           Gen                        (BurnRequest, EncoinsRequest (..), MintRequest (..), MixedRequest)
+import           PlutusTx.Prelude           (Group (inv), emptyByteString, sha2_256, zero)
+import           Prelude                    hiding (readFile)
+import           System.Random              (randomIO)
 import           Test.Hspec                 (Expectation, describe, hspec, it)
-import           Test.QuickCheck            (Testable (property), withMaxSuccess, Arbitrary (arbitrary), generate)
-import qualified Ledger.Value as Value
+import           Test.QuickCheck            (Arbitrary (arbitrary), Testable (property), generate)
 
 runScriptTest :: IO ()
 runScriptTest = do
@@ -52,13 +51,13 @@ runScriptTest = do
             let withMPTest :: forall req. EncoinsRequest req => EncoinsMode -> req -> Expectation
                 withMPTest = mintingPolicyTest ledgerParams verifierPKH verifierPrvKey
             describe "Wallet mode" $ do
-                it "Mint" $ property $ withMaxSuccess 10 $ withMPTest @MintRequest  WalletMode
-                it "Burn" $ property $ withMaxSuccess 10 $ withMPTest @BurnRequest  WalletMode
-                it "Mix"  $ property $ withMaxSuccess 10 $ withMPTest @MixedRequest WalletMode
-            -- describe "Ledger mode" $ do
-            --     it "Mint" $ property $ withMaxSuccess 10 $ withMPTest @MintRequest  LedgerMode
-            --     it "Burn" $ property $ withMaxSuccess 10 $ withMPTest @BurnRequest  LedgerMode
-            --     it "Mix"  $ property $ withMaxSuccess 10 $ withMPTest @MixedRequest LedgerMode
+                it "Mint" $ property $ withMPTest @MintRequest  WalletMode
+                it "Burn" $ property $ withMPTest @BurnRequest  WalletMode
+                it "Mix"  $ property $ withMPTest @MixedRequest WalletMode
+            describe "Ledger mode" $ do
+                it "Mint" $ property $ withMPTest @MintRequest  LedgerMode
+                it "Burn" $ property $ withMPTest @BurnRequest  LedgerMode
+                it "Mix"  $ property $ withMPTest @MixedRequest LedgerMode
 
 ledgerValidatorTest :: Params -> BuiltinByteString -> Expectation
 ledgerValidatorTest ledgerParams verifierPKH = do
@@ -85,8 +84,9 @@ mintingPolicyTest ledgerParams verifierPKH verifierPrvKey mode (extractRequest -
 
         ps            = map (\i -> if i < 0 then Burn else Mint) req
         secrets       = zipWith (\i g -> Secret g (toFieldElement i)) req gammas
-        v             = sum $ map (fst . fromSecret bulletproofSetup) secrets
-        par           = (ledgerAddress, changeAddress, 2*protocolFee mode v) :: TxParams
+        v             = sum req
+        fees          = 2 * protocolFee mode v
+        par           = (ledgerAddress, changeAddress, fees) :: TxParams
         bp            = parseBulletproofParams $ sha2_256 $ toBytes par
         inputs        = zipWith (\(_, bs) p -> (bs, p)) (map (fromSecret bulletproofSetup) secrets) ps
         (_, _, proof) = bulletproof bulletproofSetup bp secrets ps randomness
@@ -95,9 +95,37 @@ mintingPolicyTest ledgerParams verifierPKH verifierPrvKey mode (extractRequest -
         redOnChain = mkEncoinsRedeemerOnChain verifierPrvKey red
 
         beaconSymbol = beaconCurrencySymbol encoinsParams
-        beaconWithFee = singleton beaconSymbol beaconTokenName 1 <> protocolFeeValue mode v
-        txMint =  Value . PAM.fromList . (:[]) . (encoinsCs,) . PAM.fromList . sortBy (compare `on` fst) 
-            . fmap (bimap TokenName polarityToInteger) $ inputs
+        beacon = singleton beaconSymbol beaconTokenName 1
+        mint = sortBy (compare `on` fst) $ bimap TokenName polarityToInteger <$> inputs
+        txMint = Value . PAM.fromList . (:[]) . (encoinsCs,) . PAM.fromList $ mint
+        val = lovelaceValueOf $ v * 1_000_000
+        deposits = case mode of
+            WalletMode -> 0
+            LedgerMode -> depositMultiplier * sum (polarityToInteger <$> ps)
+        valDeposits = lovelaceValueOf $ deposits * 1_000_000
+
+        tokenNameToVal name = Value . PAM.fromList . (:[]) . (encoinsCs,) $ PAM.fromList [(name, 1)]
+        ledgerInVal = filter (not . isZero) $ (\(name, i) -> if i == -1 then minTxOutValueInLedger <> tokenNameToVal name else zero) <$> mint
+        ledgerOutVal = filter (not . isZero) $ (\(name, i) -> if i == 1 then minTxOutValueInLedger <> tokenNameToVal name else zero) <$> mint
+        (ledgerInVal', ledgerOutVal') = balanceLedgerInsOuts ledgerInVal ledgerOutVal (val <> valDeposits <> txMint)
+        
+        ledgerIns
+            | mode == WalletMode && v > 0 = [TxInInfo ref $ TxOut ledgerAddress minTxOutValueInLedger (OutputDatum (Datum $ toBuiltinData ())) Nothing]
+            | mode == WalletMode = [TxInInfo ref $ TxOut ledgerAddress (inv val <> minTxOutValueInLedger) (OutputDatum (Datum $ toBuiltinData ())) Nothing]
+            | otherwise = ledgerInVal' <&> \v -> TxInInfo ref $ TxOut ledgerAddress v (OutputDatum (Datum $ toBuiltinData ())) Nothing
+
+        ledgerOuts
+            | mode == WalletMode && v > 0 = [TxOut ledgerAddress (val <> minTxOutValueInLedger) (OutputDatum (Datum $ toBuiltinData ())) Nothing]
+            | mode == WalletMode = [TxOut ledgerAddress minTxOutValueInLedger (OutputDatum (Datum $ toBuiltinData ())) Nothing]
+            | otherwise = ledgerOutVal' <&> \v -> TxOut ledgerAddress v (OutputDatum (Datum $ toBuiltinData ())) Nothing
+
+        walletIns
+            | v + fees + deposits > 0 = [TxInInfo ref $ TxOut changeAddress (lovelaceValueOf $ (v + fees + deposits) * 1_000_000) NoOutputDatum Nothing ]
+            | otherwise = []
+
+        walletOuts
+            | v + fees + deposits > 0 = []
+            | otherwise = [TxOut changeAddress (lovelaceValueOf $ -(v + fees + deposits) * 1_000_000) NoOutputDatum Nothing]
 
     testMintingPolicy
         ledgerParams
@@ -105,27 +133,26 @@ mintingPolicyTest ledgerParams verifierPKH verifierPrvKey mode (extractRequest -
         (Aiken redOnChain)
         (ScriptContext
             emptyInfo
-                { txInfoReferenceInputs = [TxInInfo (TxOutRef (TxId "") 0) (TxOut ledgerAddress beaconWithFee NoOutputDatum Nothing)]
+                { txInfoReferenceInputs = [TxInInfo ref (TxOut ledgerAddress beacon NoOutputDatum Nothing)]
                 , txInfoMint = txMint
-                , txInfoOutputs = [TxOut ledgerAddress (lovelaceValueOf $ v * 1_000_000) (OutputDatum (Datum $ toBuiltinData ())) Nothing]
+                , txInfoInputs = ledgerIns <> walletIns
+                , txInfoOutputs = ledgerOuts <> walletOuts
                 }
             (Minting encoinsCs))
 
-genEncoinsParams :: BuiltinByteString -> IO EncoinsProtocolParams
-genEncoinsParams verifierPKH = generate $ (,,verifierPKH) <$> arbitrary <*> arbitrary
+balanceLedgerInsOuts :: [Value] -> [Value] -> Value -> ([Value], [Value])
+balanceLedgerInsOuts ins outs vWithDeposits
+    | isZero delta = (ins, outs)
+    | delta `leq` zero = case (ins, outs) of
+        (is, o:os) -> (is, o <> inv (adaOnlyValue delta)  : os)
+        (i:is, []) -> (minTxOutValueInLedger <> i : is, [minTxOutValueInLedger <> inv (adaOnlyValue delta)])
+        ([], []) -> ([], [])
+    | otherwise = case (ins, outs) of
+        (i:is, os) -> (i <> adaOnlyValue delta : is, os)
+        ([], o:os) -> ([minTxOutValueInLedger <> adaOnlyValue delta], minTxOutValueInLedger <> o : os)
+        ([], []) -> ([], [])
+    where
+        delta = mconcat outs <> inv (mconcat ins <> vWithDeposits)
 
-emptyInfo :: TxInfo
-emptyInfo = TxInfo {
-    txInfoInputs = [],
-    txInfoReferenceInputs = [],
-    txInfoOutputs = [],
-    txInfoFee = zero,
-    txInfoMint = zero,
-    txInfoDCert = [],
-    txInfoWdrl = empty,
-    txInfoValidRange = always,
-    txInfoSignatories = [],
-    txInfoRedeemers = empty,
-    txInfoData = empty,
-    txInfoId = TxId emptyByteString
-}
+ref :: TxOutRef
+ref = TxOutRef (TxId "") 0
