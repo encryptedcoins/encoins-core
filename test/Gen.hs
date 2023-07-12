7cb7f61a47bfee876af10699fca1fc5bb927461f
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-07-12 07:22:50 +0300

    Fix generators

--- test/Gen.hs
+++ test/Gen.hs
@@ -6,13 +6,15 @@
 
 module Gen where
 
-import           Control.Monad        (replicateM)
-import           ENCOINS.Core.OnChain (EncoinsProtocolParams)
-import           Plutus.V2.Ledger.Api (BuiltinByteString)
-import           Test.QuickCheck      (Arbitrary (..), choose, generate)
+import           Control.Monad              (replicateM)
+import           ENCOINS.Core.OnChain       (EncoinsProtocolParams)
+import           Plutus.V2.Ledger.Api       (BuiltinByteString)
+import           PlutusAppsExtra.Test.Utils (genTxOutRef)
+import           Test.QuickCheck            (Arbitrary (..), choose, generate)
 
 genEncoinsParams :: BuiltinByteString -> IO EncoinsProtocolParams
-genEncoinsParams verifierPKH = generate $ (,,verifierPKH) <$> arbitrary <*> arbitrary
+genEncoinsParams verifierPKH = (,,verifierPKH) <$> genTxOutRef <*> genTxOutRef
+
 class EncoinsRequest a where
     extractRequest :: a -> [Integer]
 
@@ -26,7 +28,7 @@ newtype MintRequest = MintRequest [Integer]
 instance Arbitrary MintRequest where
     arbitrary = do
         MixedRequest req <- arbitrary
-        pure $ MintRequest $ map abs $ filter (/= 0) req
+        pure $ MintRequest $ map abs req
 
 newtype BurnRequest = BurnRequest [Integer]
     deriving (Show, Eq)
@@ -35,11 +37,16 @@ newtype BurnRequest = BurnRequest [Integer]
 instance Arbitrary BurnRequest where
     arbitrary = do
         MintRequest req <- arbitrary
-        pure $ BurnRequest $ map negate $ filter (/= 0) req
+        pure $ BurnRequest $ map negate req
 
 newtype MixedRequest = MixedRequest [Integer]
     deriving (Show, Eq)
     deriving newtype EncoinsRequest
 
 instance Arbitrary MixedRequest where
-    arbitrary = fmap MixedRequest $ choose (1,5) >>= (`replicateM` choose (-100, 100))
+    arbitrary = fmap MixedRequest $ do
+        l <- choose (1,5)
+        replicateM l $ do
+            ada <- choose (1, 100)
+            b <- arbitrary
+            pure $ if b then ada else negate ada
