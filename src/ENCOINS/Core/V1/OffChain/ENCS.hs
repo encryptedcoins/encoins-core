fb5b3193c338269fbd04f7179cc64fafef540ba9
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-11-15 21:54:07 +0300

    Add tokens utxo constraint to delegateTx

--- src/ENCOINS/Core/V1/OffChain/ENCS.hs
+++ src/ENCOINS/Core/V1/OffChain/ENCS.hs
@@ -11,24 +11,30 @@
 
 module ENCOINS.Core.V1.OffChain.ENCS where
 
-import           Data.Functor                             (($>))
-import           Data.Text                                (Text)
-import           Ledger.Address                           (PaymentPubKeyHash(..), stakingCredential)
-import           Plutus.V2.Ledger.Api                     hiding (singleton)
-import           PlutusTx.Prelude                         hiding (mapM, (<$>), (<>))
+import           Data.Functor                         (($>))
+import           Data.Text                            (Text)
+import           Ledger.Address                       (stakingCredential, PaymentPubKeyHash (..))
+import           Plutus.V2.Ledger.Api                 hiding (singleton)
+import           PlutusTx.Prelude                     hiding (mapM, (<$>))
 
+import           Ledger.Tx.Constraints.TxConstraints  (TxOutDatum (..))
+import qualified Plutus.Script.Utils.Ada              as P
+import           Plutus.Script.Utils.Value            (singleton)
 import           PlutusAppsExtra.Constraints.OffChain
-import           PlutusAppsExtra.Types.Tx                 (TransactionBuilder)
-import qualified Plutus.Script.Utils.Ada as P
-import           Ledger.Tx.Constraints.TxConstraints (TxOutDatum (..))
+import           PlutusAppsExtra.Types.Tx             (TransactionBuilder)
 
-delegateTx :: Address -- ^ Address of the delegator
+delegateTx :: CurrencySymbol
+           -> TokenName
+           -> Address -- ^ Address of the delegator
            -> Text    -- ^ IP address of the relay-server
            -> TransactionBuilder ()
-delegateTx changeAddr ipAddr = do
-    let delegLabel = encodeUtf8 $ toBuiltin ("ENCS Delegation" :: Text)
-        ipAddrBS   = encodeUtf8 $ toBuiltin ipAddr
-    utxoProducedTx changeAddr (P.lovelaceValueOf 5_000_000) (Just $ TxOutDatumInline $ Datum $ toBuiltinData (delegLabel, ipAddrBS))
-    case stakingCredential changeAddr of
-        Just (StakingHash (PubKeyCredential pkh)) -> mustBeSignedByTx $ PaymentPubKeyHash pkh
-        _ -> failTx "delegateTx" "The change address is not associated with a staking public key!" Nothing $> ()
+delegateTx cs tokenName changeAddr ipAddr = do
+    let delegLabel1 = encodeUtf8 $ toBuiltin ("ENCOINS" :: Text)
+        delegLabel2 = encodeUtf8 $ toBuiltin ("Delegate" :: Text)
+        ipAddrBS    = encodeUtf8 $ toBuiltin ipAddr
+    pkhBS <- case stakingCredential changeAddr of
+        Just (StakingHash (PubKeyCredential (PubKeyHash pkh))) -> return pkh
+        _ -> failTx "delegateTx" "The change address is not associated with a staking public key!" Nothing $> ""
+    let dat = TxOutDatumInline $ Datum $ toBuiltinData [delegLabel1, delegLabel2, pkhBS, ipAddrBS]
+    utxoProducedTx changeAddr (P.lovelaceValueOf 5_000_000 <> singleton cs tokenName 1) (Just dat)
+    mustBeSignedByTx (PaymentPubKeyHash (PubKeyHash pkhBS))
